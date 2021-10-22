13b5bb3a7afb076676e289d23205a4be1c6029b3
noname-03 <fahrurrozi.arul3@gmail.com>
2021-10-22 12:54:19 -0700

    datatables crud

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DataTableAjaxCRUDController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('ajax-crud-datatable', [DataTableAjaxCRUDController::class, 'index']);
+Route::post('store-company', [DataTableAjaxCRUDController::class, 'store']);
+Route::post('edit-company', [DataTableAjaxCRUDController::class, 'edit']);
+Route::post('delete-company', [DataTableAjaxCRUDController::class, 'destroy']);
